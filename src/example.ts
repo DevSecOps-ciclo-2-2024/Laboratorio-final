4f73dbdb102957ee2743fcd3c73ae0e195fe8b1d
ArielAndresMarihuenMatus <marihuen3@gmail.com>
2024-06-27 17:14:33 -0400

    Creación de pipeline y sast con sonar parte 2

--- src/example.ts
+++ src/example.ts
@@ -1,7 +1,6 @@
-// Ejemplo de código vulnerable a buffer overflow en TypeScript
-function copyData(source: string) {
-    const dest: string[] = new Array(10);
-    for (let i = 0; i < source.length; i++) {
-        dest[i] = source[i];
-    }
+// Ejemplo de uso inseguro de setTimeout con eval
+function executeCodeAfterDelay(code: string, delay: number) {
+    setTimeout(() => {
+        eval(code); // Uso de eval dentro de setTimeout
+    }, delay);
 }
